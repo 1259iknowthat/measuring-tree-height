c63efb07a053c0bdb5290c74e9526a327d09079e
1259iknowthat <huaduy1259@gmail.com>
2023-05-21 19:13:31 +0700

    update width of collumns in graph

--- graph.py
+++ graph.py
@@ -10,7 +10,7 @@ tick_label = ['AVL tree', 'Red-Black tree']
   
 
 plt.bar(left, height, tick_label = tick_label,
-        width = 0.8, color = ['red', 'green'])
+        width = 0.5, color = ['red', 'green'])
   
 
 plt.xlabel('Trees')
